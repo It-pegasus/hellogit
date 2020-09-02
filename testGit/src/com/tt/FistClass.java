f9a2a0b258d2bbca31eabe77301a1b76479ea32b
IT_pegasus <857342969@qq.com>
2020-09-02 22:47:32 +0800

    更新了git

--- testGit/src/com/tt/FistClass.java
+++ testGit/src/com/tt/FistClass.java
@@ -1,7 +1,16 @@
 package com.tt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FistClass {
     public static void main(String[] args) {
         System.out.println("你好哇");
+        List<String> list=new ArrayList<>();
+        list.add("你好");
+        for (String s : list) {
+            System.out.println(s);
+        }
     }
 }
+
